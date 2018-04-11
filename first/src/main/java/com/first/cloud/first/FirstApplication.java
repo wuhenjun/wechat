1277d15cf8246d5ebc5bcb7b1ea1d796fc27afba
wuhenjun <625601781@qq.com>
2018-04-12 00:05:49 +0800

    XIUGAI

--- first/src/main/java/com/first/cloud/first/FirstApplication.java
+++ first/src/main/java/com/first/cloud/first/FirstApplication.java
@@ -1,9 +1,13 @@
 package com.first.cloud.first;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableAutoConfiguration
 public class FirstApplication {
 
 	public static void main(String[] args) {
